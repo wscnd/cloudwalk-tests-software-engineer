dc2885caf69392e02023f3cb27f810503b78e92d
wscnd <72019998+wscnd@users.noreply.github.com>
2024-06-29 14:09:03 -0300

    feat(matches): identify match boundaries with InitGame events

--- internal/logparser/parser.go
+++ internal/logparser/parser.go
@@ -3,39 +3,68 @@ package logparser
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"strings"
 )
 
 type LogParser struct {
-	logfile *os.File
+	logfile io.Reader
+	matches *Match
 }
 
-func NewLogParser(logFile *os.File) *LogParser {
+func NewLogParser(logFile io.Reader, match *Match) *LogParser {
 	return &LogParser{
 		logfile: logFile,
+		matches: match,
 	}
 }
 
 type Match struct {
-	Matches int
+	Matches [][]string
 }
 
-func (lp *LogParser) detectMatches() int {
+func (lp *LogParser) detectMatches() error {
 	scanner := bufio.NewScanner(lp.logfile)
 
-	var count Match
+	var lines []string
+	var inMatch bool = false
 	for scanner.Scan() {
 		line := scanner.Text()
 		if strings.Contains(line, "InitGame:") {
-			count.Matches++
+			// we are in a game
+			if inMatch {
+				lp.matches.Matches = append(lp.matches.Matches, lines)
+				lines = nil
+				inMatch = false
+			} else {
+				inMatch = true
+			}
+		} else {
+			// lines with "---" are ignored
+			if !strings.Contains(line, "---") {
+				inMatch = true
+				lines = append(lines, line)
+			}
 		}
 	}
-	return count.Matches
+	if len(lines) != 0 {
+		lp.matches.Matches = append(lp.matches.Matches, lines)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func Run(file *os.File) {
-	parser := NewLogParser(file)
+	m := &Match{
+		Matches: make([][]string, 0, 21),
+	}
+
+	parser := NewLogParser(file, m)
 	matchesDetected := parser.detectMatches()
 
 	fmt.Printf("%+v matches found!\n", matchesDetected)
