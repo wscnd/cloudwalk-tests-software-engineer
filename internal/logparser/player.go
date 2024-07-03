9f77ac6744004d84f9c6c5bcc91241738e7c320c
wscnd <72019998+wscnd@users.noreply.github.com>
2024-07-03 08:18:11 -0300

    feat(player): add model methods

--- internal/logparser/player.go
+++ internal/logparser/player.go
@@ -1,7 +1,27 @@
 package logparser
 
-type PlayerData struct {
-	Name   string `json:"name"`
-	Kills  int    `json:"kills"`
-	Deaths int    `json:"deaths"`
+type Player struct {
+	ID          string         `json:"-"`
+	Name        string         `json:"name"`
+	Kills       int            `json:"kills"`
+	DeathCauses []meansOfDeath `json:"deathCauses"`
+}
+
+func (p *Player) RecordKill(victim *Player) {
+	// Handle the case where a player kills another player
+	p.Kills++
+}
+
+func (p *Player) RecordDeath(mean meansOfDeath) {
+	p.DeathCauses = append(p.DeathCauses, mean)
+}
+
+func (p *Player) UpdateNickname(name string) {
+	p.Name = name
+}
+
+func NewPlayer(id string) *Player {
+	p := new(Player)
+	p.ID = id
+	return p
 }
